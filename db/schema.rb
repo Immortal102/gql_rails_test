e001d2f0c7fa283bb66154545d31609c780a22b0
Dmitry Stepanenko <sdm102@yandex.ua>
2018-02-07 11:07:15 +0200

    Update users table by adding password_digest column to be able to use #has_secure_password for authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205130335) do
+ActiveRecord::Schema.define(version: 20180207090350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20180205130335) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
